5d2c0c24b4bb0ff16c25067fdddcae630e6a7f4e
URVIL2512 <urvilsolanki2004@gmail.com>
2025-11-15 14:03:53 +0530

    Add delete route to feedback routes

--- server/routes/feedbackRoutes.js
+++ server/routes/feedbackRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import authMiddleware from '../middleware/authMiddleware.js';
-import { createFeedback, getFeedbacks } from '../controllers/feedbackController.js';
+import { createFeedback, getFeedbacks, deleteFeedback } from '../controllers/feedbackController.js';
 import { exportToCSV } from '../controllers/exportController.js';
 
 const router = express.Router();
@@ -8,6 +8,7 @@ const router = express.Router();
 router.post('/', authMiddleware, createFeedback);
 router.get('/', authMiddleware, getFeedbacks);
 router.get('/export', authMiddleware, exportToCSV);
+router.delete('/:id', authMiddleware, deleteFeedback);
 
 export default router;
 
